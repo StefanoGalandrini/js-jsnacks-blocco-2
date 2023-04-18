dae6d2e618a56eb380d51fc758e0e09a0e280a76
StefanoGalandrini <stefe68@gmail.com>
2023-04-18 16:51:02 +0200

    Add initial html, css and javascript files

---
+++ jsnack2/script.js
@@ -0,0 +1,33 @@
+/*
+Generatore di “nomi cognomi” casuali:
+il Grande Gatsby ha una lista di nomi e una lista di cognomi,
+e da queste vuole generare
+una falsa lista di invitati con nome e cognome.
+*/
+
+// create arrays for first names and last names
+const arrFirstName = [
+	"Stefano",
+	"Laura",
+	"Daniele",
+	"Giorgia",
+	"Elena",
+	"Francesco",
+	"Lorenzo",
+	"Lisa",
+	"Cristina",
+	"Gianfranco",
+];
+
+const arrLastName = [
+	"Rossi",
+	"Bianchi",
+	"Verdi",
+	"Viola",
+	"Rosato",
+	"De Grigis",
+	"Neri",
+	"Salmone",
+	"Dorati",
+	"Azzurri",
+];
