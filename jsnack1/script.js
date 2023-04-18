30e54e5ba0a3f9412c6d6e1b0d481beddc5558e4
StefanoGalandrini <stefe68@gmail.com>
2023-04-18 16:25:08 +0200

    Add javascript, add fixed html

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,3 +3,37 @@ Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.
 */
+
+// create empty array and global DOM variables
+let arrValues = [];
+let inputButton = document.querySelector(".btn-input");
+let resultDiv = document.querySelector(".values");
+
+// reset
+resultDiv.innerHTML = ``;
+
+inputButton.addEventListener("click", function () {
+	// reset
+	resultDiv.innerHTML = ``;
+	arrValues = [];
+
+	// parse Array to push the odd input values
+	for (let i = 1; i <= 6; i++) {
+		let inputNum = parseInt(document.querySelector("#number" + i).value);
+
+		// check if the input is a number and it is odd
+		if (!isNaN(inputNum) && inputNum % 2 !== 0) {
+			arrValues.push(inputNum);
+		}
+	}
+
+	let noOddNumbers = "";
+
+	if (arrValues.length == 0) {
+		noOddNumbers = "Nessuno";
+	} else {
+		noOddNumbers = "";
+	}
+	// inject content in html div
+	resultDiv.innerHTML = `${noOddNumbers} ${arrValues.join(", ")}`;
+});
